8af79614ef81c5b2a7d1bfd67e0cea518560b24a
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2016-01-22 21:20:07 +0100

    Don't pollute stdout

--- src/services/ruter.spec.js
+++ src/services/ruter.spec.js
@@ -4,8 +4,7 @@ describe('ruter api', () => {
   it('should not crash', done => {
     getDepartures(stops.dalenenga).then(data => {
       //do something with data
-      console.log(data);
-      done();
-    });
+      done()
+    })
   })
 })
