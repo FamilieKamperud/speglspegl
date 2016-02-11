562832c6fdd4fef51fa464f278ffacc31a0a7237
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-02-11 21:25:56 +0100

    Production build configurations

---
+++ webpack.prod.config.js
@@ -0,0 +1,48 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/speglspegl/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.ProvidePlugin({
+      'Promise': 'exports?global.Promise!es6-promise'
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!stylus-loader',
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.(ttf)$/,
+        loader: 'url-loader?limit=8192'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
