6deb85fcb57def3d8bf6b52dc2a73797bbb3c6a1
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-02-13 23:35:44 +0100

    Updated require after file rename

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -13,8 +13,8 @@ export default function configureStore(initialState) {
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers')
+    module.hot.accept('../reducers/rootReducer', () => {
+      const nextRootReducer = require('../reducers/rootReducer')
       store.replaceReducer(nextRootReducer)
     })
   }
