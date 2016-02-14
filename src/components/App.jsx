0d639bae1bafa4de8cb67c4fe076f4d2c998fe0c
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-02-14 21:42:51 +0100

    Created clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import RuterSchedule from './RuterSchedule'
+import Clock from './Clock'
 
 import './app.styl'
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="app">
         <RuterSchedule />
+        <Clock />
       </div>
     )
   }
---
+++ src/components/Clock.jsx
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react'
+import moment from 'moment'
+
+import './clock.styl'
+
+export default class Clock extends React.Component{
+  constructor(props) {
+    super(props)
+    this.interval = setInterval(this.tick, 1000)
+    const time = moment()
+    this.state = { time }
+  }
+  tick = () => {
+    this.setState({ time: moment()})
+  };
+
+  render () {
+    return (
+      <div className="clock">
+        <h5 className="clock-date">{this.state.time.format('dddd Do MMMM')}</h5>
+        <h1 className="clock-time">{this.state.time.format('HH:mm')}</h1>
+      </div>
+    );
+  }
+}
