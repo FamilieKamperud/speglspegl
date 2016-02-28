b4db2397264997c9b026a6f0120bf50d53a6bfc1
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-02-28 21:28:53 +0100

    Changed format of Yr data

--- src/services/yr.js
+++ src/services/yr.js
@@ -1,11 +1,12 @@
 import fetch from 'isomorphic-fetch';
 import X2JSFuglyAPI from 'x2js';
 const x2js = new X2JSFuglyAPI();
-import receiveWeather from '../actions';
+import { receiveWeather } from '../actions';
 
 
 export const postalCode = '0566';
 const WEATHER_ENTRIES = 3;
+export const YR_SYMBOL_URL = 'https://crossorigin.me/http://symbol.yr.no/grafikk/sym/b100/';
 
 export const fetchWeatherInfo = (dispatch) =>{
   getWeather(postalCode).then(weather =>{
@@ -17,22 +18,28 @@ export function getWeather(location){
   return fetch(`https://crossorigin.me/http://www.yr.no/sted/Norge/postnummer/${location}/varsel.xml`)
     .then(function(response) {
       return response.text();
+    })
+    .then(function(response){
+      return parseWeatherInfo(response);
     });
 }
+// TODO effective temperature
+// W = 13,12 + 0,6215T - 11,37V^0,16 + 0,3965T*V^0,16
+// T = temperature i C, V = vinhastighet km/t
 export function parseWeatherInfo(xmlWeatherData){
   const weatherDocument = x2js.xml2js(xmlWeatherData);
-  let parsedWeatherInfo = {};
+  let parsedWeatherInfo = [];
   weatherDocument.weatherdata.forecast.tabular.time.slice(0, WEATHER_ENTRIES).map(time => {
     let parsedTime = {
-      symbol: time.symbol._var,
+      from: time._from,
+      to: time._to,
+      symbol: YR_SYMBOL_URL+time.symbol._var+'.png',
       period: time._period,
       precipitation: time.precipitation._value,
       windSpeed: time.windSpeed._mps,
       temperature: time.temperature._value
     };
-    const fromTime = time._from;
-    const toTime = time._to;
-    parsedWeatherInfo[`${fromTime} ${toTime}`] = parsedTime;
+    parsedWeatherInfo.push(parsedTime);
 
   });
   return parsedWeatherInfo;
--- src/services/yr.spec.js
+++ src/services/yr.spec.js
@@ -1,7 +1,7 @@
 /*eslint-env node, mocha */
 /*global expect */
 
-import { getWeather, postalCode, parseWeatherInfo } from './yr';
+import { getWeather, postalCode, parseWeatherInfo, YR_SYMBOL_URL } from './yr';
 import mockGetWeatherResponse from 'raw!../mockData/mockGetWeatherResponse.xml';
 
 describe('yr api', () => {
@@ -16,29 +16,35 @@ describe('yr api', () => {
   it('should parse data appropriately', ()=> {
     const result = parseWeatherInfo(mockGetWeatherResponse);
     expect(Object.keys(result).length).to.equal(3);
-    const resultShouldBe = {
-      '2016-02-21T13:00:00 2016-02-21T18:00:00': {
+    const resultShouldBe = [
+      {
+        from: '2016-02-21T13:00:00',
+        to: '2016-02-21T18:00:00',
         period: '2',
-        symbol: '04',
+        symbol: YR_SYMBOL_URL+'04.png',
         precipitation: '0',
         windSpeed: '2.0',
         temperature: '1'
       },
-      '2016-02-21T18:00:00 2016-02-22T00:00:00': {
+      {
+        from: '2016-02-21T18:00:00',
+        to: '2016-02-22T00:00:00',
         period: '3',
-        symbol: 'mf/03n.44',
+        symbol: YR_SYMBOL_URL+'mf/03n.44.png',
         precipitation: '0',
         windSpeed: '1.4',
         temperature: '0'
       },
-      '2016-02-22T00:00:00 2016-02-22T06:00:00':{
+      {
+        from: '2016-02-22T00:00:00',
+        to: '2016-02-22T06:00:00',
         period: '0',
-        symbol: 'mf/03n.47',
+        symbol: YR_SYMBOL_URL+'mf/03n.47.png',
         precipitation: '0',
         windSpeed: '1.6',
         temperature: '-2'
       }
-    };
+    ];
     expect(result).to.deep.equal(resultShouldBe);
   });
 });
