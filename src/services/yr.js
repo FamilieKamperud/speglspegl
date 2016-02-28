42c73238fbe7015e71866748b7e2d59ce1234d5e
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-02-28 22:51:38 +0100

    Changed weather icon from png to svg

--- src/services/yr.js
+++ src/services/yr.js
@@ -6,7 +6,7 @@ import { receiveWeather } from '../actions';
 
 export const postalCode = '0566';
 const WEATHER_ENTRIES = 3;
-export const YR_SYMBOL_URL = 'https://crossorigin.me/http://symbol.yr.no/grafikk/sym/b100/';
+export const YR_SYMBOL_URL = 'https://crossorigin.me/http://symbol.yr.no/grafikk/sym/svg/';
 
 export const fetchWeatherInfo = (dispatch) =>{
   getWeather(postalCode).then(weather =>{
@@ -23,9 +23,7 @@ export function getWeather(location){
       return parseWeatherInfo(response);
     });
 }
-// TODO effective temperature
-// W = 13,12 + 0,6215T - 11,37V^0,16 + 0,3965T*V^0,16
-// T = temperature i C, V = vinhastighet km/t
+
 export function parseWeatherInfo(xmlWeatherData){
   const weatherDocument = x2js.xml2js(xmlWeatherData);
   let parsedWeatherInfo = [];
@@ -33,7 +31,7 @@ export function parseWeatherInfo(xmlWeatherData){
     let parsedTime = {
       from: time._from,
       to: time._to,
-      symbol: YR_SYMBOL_URL+time.symbol._var+'.png',
+      symbol: YR_SYMBOL_URL+time.symbol._var+'.svg',
       period: time._period,
       precipitation: time.precipitation._value,
       windSpeed: time.windSpeed._mps,
