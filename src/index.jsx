51da60c39da54488858020ce0157eb64f12e2336
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-02-28 21:32:19 +0100

    Fetching data from Yr and adding to page

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import './debug'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import './debug';
 
-import App from './components/App'
-import configureStore from './store/configureStore'
-import { fetchBusDepartures } from './services/ruter'
-import { fetchWeatherInfo } from './services/yr'
+import App from './components/App';
+import configureStore from './store/configureStore';
+import { fetchBusDepartures } from './services/ruter';
+import { fetchWeatherInfo } from './services/yr';
 
-const store = configureStore()
+const store = configureStore();
 
-fetchBusDepartures(store.dispatch)
+fetchBusDepartures(store.dispatch);
 setInterval(()=>{
-  fetchBusDepartures(store.dispatch)
-}, 10000)
-fetchWeatherInfo(store.dispatch)
+  fetchBusDepartures(store.dispatch);
+}, 10000);
+fetchWeatherInfo(store.dispatch);
 setInterval(()=>{
-  fetchWeatherInfo(store.dispatch)
-}, 3600000)
+  fetchWeatherInfo(store.dispatch);
+}, 3600000);
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
