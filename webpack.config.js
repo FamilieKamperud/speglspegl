3932d62a42cf48309cb3fd348353c2e1e3640d84
Hanna Kamperud <hanna.kamperud@gmail.com>
2016-05-01 17:50:13 +0200

    Removed warning in chrome

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack')
 var poststylus = require('poststylus')
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: '#cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
     './src/index'
